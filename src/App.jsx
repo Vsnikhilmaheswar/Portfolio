e53183b850bd23d55a0eddb2ee9cf10977118ad5
Vsnikhilmaheswar <nikhilmaheswar12@gmail.com>
2025-01-01 00:03:29 +0530

    xx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import './App.css'
 import Projects from './components/Projects.jsx'
-import Resume  from './components/resume.jsx'
-import Contact from './components/Contact.jsx'
+import Resume  from './components/Resume.jsx'
+
 import Timeline from './components/Timeline.jsx'
 function App() {
   const [count, setCount] = useState(0)
